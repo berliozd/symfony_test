eb90308442e56305ca9fc42a9547b48df68be042
Didier Berlioz <berliozd@gmail.com>
2015-09-08 00:12:04 +0200

    testing validators

--- symfony_demo/src/AppBundle/Controller/ArticleController.php
+++ symfony_demo/src/AppBundle/Controller/ArticleController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Author;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,6 +24,24 @@ class ArticleController extends Controller {
      */
     public function listAction(Request $_request)
     {
+
+        $author = new Author();
+
+        // ... do something to the $author object
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($author);
+
+        if (count($errors) > 0) {
+            /*
+             * Uses a __toString method on the $errors variable which is a
+             * ConstraintViolationList object. This gives us a nice string
+             * for debugging.
+             */
+            $errorsString = (string) $errors;
+            return new Response($errorsString);
+        }
+
         $articles = array(
             array('title' => 'titre1', 'authorName' => 'Victor hugo', 'body' => 'corps de aticle'),
             array('title' => 'titre2', 'authorName' => 'Victor hugozi', 'body' => 'corps de aticle 2'),
---
+++ symfony_demo/src/AppBundle/Entity/Author.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Berlioz
+ * Date: 03/09/2015
+ * Time: 23:24
+ */
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class Author {
+    /**
+     * @Assert\NotBlank()
+     */
+    public $name;
+
+}
